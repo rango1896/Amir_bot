f5305750cafb51a3cf281116d8ce50bb28fcbfef
rango1896 <noroziamir957@gmail.com>
2026-09-01 17:50:54 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import asyncio
+import core
+import game
+import tools
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "ربات زنده‌ست! 🐱"
+
+def run_web():
+    app.run(host='0.0.0.0', port=8080)
+
+async def main():
+    await core.client.start()
+    await core.load_db() # لود کردن دیتابیس از تلگرام
+    core.group_entity = await core.client.get_entity(core.TARGET_GROUP)
+    print(f"✅ گروه پیدا شد: {core.group_entity.title}")
+    print("✅ سلف‌بات Amir روشن شد!")
+    
+    await asyncio.gather(
+        game.meow_loop(),
+        game.update_name_clock(),
+        game.collect_points_loop(),
+        game.fishing_loop(),
+        tools.schedule_loop()
+    )
+
+def keep_alive():
+    import time
+    while True:
+        time.sleep(60)
+
+if __name__ == "__main__":
+    threading.Thread(target=run_web).start()
+    threading.Thread(target=keep_alive, daemon=True).start()
+    with core.client:
+        core.client.loop.run_until_complete(main())
